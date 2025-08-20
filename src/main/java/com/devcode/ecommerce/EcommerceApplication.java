bf622c29781f6ca1f34ad292b1e79b22cd206c27
Neha Eglund <nehaeglund@Nehas-MacBook-Pro.local>
2025-08-20 13:50:39 +0200

    Initial commit with my project

---
+++ src/main/java/com/devcode/ecommerce/EcommerceApplication.java
@@ -0,0 +1,31 @@
+package com.devcode.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+
+    /*
+
+    User Management
+    Register/Delete User
+    Login flow - Authorization and Authentication
+
+    Add Products
+    Create Order
+    Update Inventory
+    Make Payment
+    Send Order
+
+
+
+
+     */
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
